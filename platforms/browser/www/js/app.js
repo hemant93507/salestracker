d1a07edd10334bdfaf47b63264f62b16947a8616
danish1023 <danish1023@gmail.com>
2019-04-30 18:32:47 +0530

    debug

--- platforms/browser/www/js/app.js
+++ platforms/browser/www/js/app.js
@@ -77,58 +77,119 @@ function empty(val) {
   }
 }
 
+var reverse_location = '';
+function locationFromLatLon_success(result) {
+  var firstResult = result[0];
+  var location = firstResult.subLocality + ',' + firstResult.locality + ',' + firstResult.subAdministrativeArea;
+  reverse_location = location;
+}
+function locationFromLatLon_failure(err) {
+  reverse_location = 'NA';
+}
+
 function locationFromLatLon(lat, lon) {
-  nativegeocoder.reverseGeocode(success, failure, lat, lon, { useLocale: true, maxResults: 1 });
-  function success(result) {
-    var firstResult = result[0];
-    var location = firstResult.subLocality + ',' + firstResult.locality + ',' + firstResult.subAdministrativeArea;
-    return location;
-  }
-  function failure(err) {
-    return 'NA';
+  nativegeocoder.reverseGeocode(locationFromLatLon_success, locationFromLatLon_failure, lat, lon, { useLocale: true, maxResults: 1 });
+  return reverse_location;
+}
+
+function login() {
+  if ($$('#login-form')[0].checkValidity()) {
+    var username = $$('#login-form input[name=email]').val();
+    var password = $$('#login-form input[name=password]').val();
+    console.log(username);
+    var obj = {
+      username: username,
+      password: password,
+    };
+    app.request({
+      url: BaseURL + 'login',
+      method: 'POST',
+      dataType: 'json',
+      data: obj,
+      //contentType: 'application/json',
+      beforeSend: function (xhr) {
+        var spinnerOptions = { dimBackground: true };
+        SpinnerPlugin.activityStart('Loading...', spinnerOptions);
+      },
+      error: function (xhr, status) {
+        alert(statusMessage(status));
+      },
+      success: function (data, status, xhr) {
+        console.log(data);
+        if (data.ErrorCode == '0') {
+          localStorage.setItem("User", JSON.stringify(data));
+          var user_group = data.User.user_group;
+          if (user_group == '1') {
+            app.views.main.router.navigate({
+              name: 'dashboard',
+            });
+          }
+          else {
+            app.views.main.router.navigate({
+              name: 'user-dashboard',
+            });
+          }
+        }
+        else {
+          window.plugins.toast.show(data.ErrorMessage, 'long', 'bottom');
+        }
+      },
+      complete: function (xhr, status) {
+        SpinnerPlugin.activityStop();
+      }
+    })
   }
 }
 
-  function login() {
-    if ($$('#login-form')[0].checkValidity()) {
-      var username = $$('#login-form input[name=email]').val();
-      var password = $$('#login-form input[name=password]').val();
-      console.log(username);
+function logout() {
+  localStorage.removeItem("User");
+  app.views.main.router.navigate('/');
+}
+
+function testMe() {
+  console.log('test me is running');
+}
+
+function shareApp() {
+  if (app.device.android) {
+    var url = 'https://play.google.com/store/apps/details?id=com.techstreet.salestracker';
+  }
+  if (app.device.ios) {
+    var url = 'https://play.google.com/store/apps/details?id=com.techstreet.salestracker';
+  }
+  var options = {
+    url: url,
+  };
+  var onSuccess = function (result) { };
+  var onError = function (msg) { };
+  window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
+}
+
+function resetPassword() {
+  app.dialog.prompt('Please Enter Your Email ID', 'Forgot Password', function (email) {
+    if (email != '') {
       var obj = {
-        username: username,
-        password: password,
+        email: email
       };
       app.request({
-        url: BaseURL + 'login',
+        url: BaseURL + 'forgotpassword',
         method: 'POST',
         dataType: 'json',
         data: obj,
         //contentType: 'application/json',
         beforeSend: function (xhr) {
-          var spinnerOptions = { dimBackground: true };
-          SpinnerPlugin.activityStart('Loading...', spinnerOptions);
+          var spinnerOptions = { dimBackground: false };
+          SpinnerPlugin.activityStart(null, spinnerOptions);
         },
         error: function (xhr, status) {
-          alert(statusMessage(status));
+          alert("Error: " + status);
         },
         success: function (data, status, xhr) {
-          console.log(data);
           if (data.ErrorCode == '0') {
-            localStorage.setItem("User", JSON.stringify(data));
-            var user_group = data.User.user_group;
-            if (user_group == '1') {
-              app.views.main.router.navigate({
-                name: 'dashboard',
-              });
-            }
-            else {
-              app.views.main.router.navigate({
-                name: 'user-dashboard',
-              });
-            }
+            app.dialog.alert("A new password has been sent to your Email ID");
           }
           else {
-            window.plugins.toast.show(data.ErrorMessage, 'long', 'bottom');
+            app.dialog.alert(data.ErrorMessage);
           }
         },
         complete: function (xhr, status) {
@@ -136,69 +197,11 @@ function locationFromLatLon(lat, lon) {
         }
       })
     }
-  }
-
-  function logout() {
-    localStorage.removeItem("User");
-    app.views.main.router.navigate('/');
-  }
-
-  function testMe() {
-    console.log('test me is running');
-  }
-
-  function shareApp() {
-    if (app.device.android) {
-      var url = 'https://play.google.com/store/apps/details?id=com.techstreet.salestracker';
-    }
-    if (app.device.ios) {
-      var url = 'https://play.google.com/store/apps/details?id=com.techstreet.salestracker';
-    }
-    var options = {
-      url: url,
-    };
-    var onSuccess = function (result) { };
-    var onError = function (msg) { };
-    window.plugins.socialsharing.shareWithOptions(options, onSuccess, onError);
-  }
-
-  function resetPassword() {
-    app.dialog.prompt('Please Enter Your Email ID', 'Forgot Password', function (email) {
-      if (email != '') {
-        var obj = {
-          email: email
-        };
-        app.request({
-          url: BaseURL + 'forgotpassword',
-          method: 'POST',
-          dataType: 'json',
-          data: obj,
-          //contentType: 'application/json',
-          beforeSend: function (xhr) {
-            var spinnerOptions = { dimBackground: false };
-            SpinnerPlugin.activityStart(null, spinnerOptions);
-          },
-          error: function (xhr, status) {
-            alert("Error: " + status);
-          },
-          success: function (data, status, xhr) {
-            if (data.ErrorCode == '0') {
-              app.dialog.alert("A new password has been sent to your Email ID");
-            }
-            else {
-              app.dialog.alert(data.ErrorMessage);
-            }
-          },
-          complete: function (xhr, status) {
-            SpinnerPlugin.activityStop();
-          }
-        })
-      }
-    });
-  }
-
-  $$('.input-end').keypress(function (e) {
-    if (e.keyCode == 13) {
-      this.blur();
-    }
   });
+}
+
+$$('.input-end').keypress(function (e) {
+  if (e.keyCode == 13) {
+    this.blur();
+  }
+});
