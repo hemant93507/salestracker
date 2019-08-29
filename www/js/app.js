b100f327de747de8e510f766bb0bab906089326c
danish1023 <danish1023@gmail.com>
2019-08-29 17:19:31 +0530

    debug

--- www/js/app.js
+++ www/js/app.js
@@ -101,15 +101,6 @@ function locationFromLatLon(lat, lon) {
 }
 
 function login() {
-  window.plugins.uniqueDeviceID.get(success, fail);
-  function success(uuid) {
-    localStorage.setItem("UUID", uuid);
-    alert(localStorage.UUID);
-  };
-  function fail() {
-    alert('error');
-  };
-  
   if ($$('#login-form')[0].checkValidity()) {
     var username = $$('#login-form input[name=email]').val();
     var password = $$('#login-form input[name=password]').val();
